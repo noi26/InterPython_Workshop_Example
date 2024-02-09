845374713d41c2b0c9d365d44f8d2e8cf2dadef7
noi26 <58683809+noi26@users.noreply.github.com>
2024-02-09 21:07:29 +0200

    Update models.py

--- lcanalyzer/models.py
+++ lcanalyzer/models.py
@@ -15,7 +15,7 @@ def load_dataset(filename):
     """Load a table from CSV file.
     
     :param filename: The name of the .csv file to load.
-    :raises Eror: when the file is not CSV/
+    :raises Error: when the file is not CSV/
                   no file was given as an inut/
                   the file or directory was not found
     :returns: pd.DataFrame with the data from the file.
