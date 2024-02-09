81c5c6d0da4724b0d47e166d53e1279812b291ac
noi26 <58683809+noi26@users.noreply.github.com>
2024-02-09 21:14:10 +0200

    Update views.py
    made changes according to issue #11

--- lcanalyzer/views.py
+++ lcanalyzer/views.py
@@ -3,12 +3,14 @@
 from matplotlib import pyplot as plt
 import pandas as pd
     
-def plotUnfolded(data,mag_col,time_col,color,marker):
+def plotUnfolded(data,mag_col,time_col,color='b',marker='.'):
     """
     Display plots of unfolded lightcurves in different bands.
     :param data: a table of observations of a single object in a single band
     :param mag_col: the name of the column with magnitudes to plot
     :param time_col: the name of the column with time stamps
+    :param color: the color of the plotted data points; default is: 'b'
+    :param marker: the marker of the plotted data points; default is: '.' 
     """
     fig = plt.figure(figsize=(7,5))
     ax = fig.add_subplot(1,1,1)
